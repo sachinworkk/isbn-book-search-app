bb6a9c63bf8b739924201515ba88c3a5322596b8
sachin <sachinworkk@gmail.com>
2025-08-13 12:35:16 +0000

    Show books when typing ISBN number in search bar
    - Added fetch books on input change in search bar
    - Added loading section when the books are being fetched
    - Added basic message when books not found
    - Added type a book name to search on first load

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { useState } from "react";
-import BookList from "./components/BookList";
 import BookSearch from "./components/BookSearch";
 
 interface Book {
+  id: string;
   authors: [];
-  imageLinks: {};
+  imageLinks: { smallThumbnail: string; thumbnail: string };
+  industryIdentifiers: { type: string; identifier: string }[];
   publishedDate: string;
   publisher: string;
   title: string;
@@ -13,18 +14,35 @@ interface Book {
 
 export default function Home() {
   const [books, setBooks] = useState<Book[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSearching, setIsSearching] = useState(false);
 
-  const handleBookSearch = (book: string) => {
-    fetch(`http://localhost:8000/books/?isbn=${book}`)
-      .then((res) => res.json())
-      .then((data) => setBooks((prev) => [...prev, data]));
+  const handleBookSearchAsync = async (book: string) => {
+    setIsLoading(true);
+    setIsSearching(true);
+
+    try {
+      const res = await fetch(`http://localhost:8000/books/?isbn=${book}`);
+      const data = await res.json();
+
+      setBooks(data?.items);
+    } catch (error) {
+      console.error("Error fetching books:", error);
+      setBooks([]);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
-    <div className="w-full h-full">
-      <form className="p-5 flex flex-col gap-4">
-        <BookSearch onBookSearch={handleBookSearch} />
-        <BookList />
+    <div className="w-full h-screen flex justify-center">
+      <form className="p-5 w-1/2 mt-8 flex flex-col gap-4">
+        <BookSearch
+          onBookSearch={handleBookSearchAsync}
+          books={books}
+          isLoading={isLoading}
+          isSearching={isSearching}
+        />
       </form>
     </div>
   );
